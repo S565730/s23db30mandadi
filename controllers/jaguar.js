4c6eaae72c68ce7241d8ab2d0f58ed096be220ef
S565730 <S565730@nwmissouri.edu>
2023-11-09 22:47:15 -0600

    commit

--- controllers/jaguar.js
+++ controllers/jaguar.js
@@ -2,18 +2,15 @@ var jaguar = require('../models/jaguar');
 // List of all jaguars
 exports.jaguar_list = async function(req, res) {
     try{
-    thejaguars = await jaguar.find();
-    res.send(thejaguars);
-    }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
-    }
-   };
-// for a specific jaguar.
-exports.jaguar_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: jaguar detail: ' + req.params.id);
+        thejaguars = await jaguar.find();
+        res.send(thejaguars);
+        }
+        catch(err){
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+        }
 };
+
 // Handle jaguar create on POST.
 exports.jaguar_create_post = function(req, res) {
  res.send('NOT IMPLEMENTED: jaguar create POST');
@@ -55,6 +52,15 @@ exports.jaguar_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
    }
-   
-  
-   
+
+// for a specificjaguar.
+exports.jaguar_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await jaguar.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
--- routes/resource.js
+++ routes/resource.js
@@ -18,3 +18,23 @@ router.get('/jaguars/:id', jaguar_controller.jaguar_detail);
 // GET request for list of all jaguar items.
 router.get('/jaguars', jaguar_controller.jaguar_list);
 module.exports = router;
+// Handle jaguar update form on PUT.
+exports.jaguar_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await jaguar.findById( req.params.id)
+// Do updates of properties
+if(req.body.jaguar_type)
+toUpdate.jaguar_type = req.body.jaguar_type;
+if(req.body.cost) toUpdate.cost = req.body.cost;
+if(req.body.size) toUpdate.size = req.body.size;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
+};
