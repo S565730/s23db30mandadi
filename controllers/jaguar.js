cbd2d47389381b9a936ccef718e4eaff8ff6da86
S565730 <S565730@nwmissouri.edu>
2023-11-15 13:59:15 -0600

    commit

--- controllers/jaguar.js
+++ controllers/jaguar.js
@@ -64,3 +64,24 @@ exports.jaguar_detail = async function(req, res) {
     res.send(`{"error": document for id ${req.params.id} not found`);
     }
     };
+// Handle jaguar update form on PUT.
+exports.jaguar_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await jaguar.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.jaguar_color)
+    toUpdate.jaguar_color = req.body.jaguar_color;
+    if(req.body.jaguar_breed) toUpdate.cost = req.body.jaguar_breed;
+    if(req.body.jaguar_price) toUpdate.size = req.body.jaguar_price;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+    }
+    };
+    
