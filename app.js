bd1295ad9de8c95935e1b64b6fe4a03c11ba6d96
S565730 <S565730@nwmissouri.edu>
2023-11-17 01:33:00 -0600

    Update app.js

--- app.js
+++ app.js
@@ -73,6 +73,7 @@ app.use('/board', boardRouter);
 app.use('/choose', chooseRouter);
 app.use('/resource', resourceRouter);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
