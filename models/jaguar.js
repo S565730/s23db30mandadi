49d041fba65801b01312ad6c45fabad91e86501e
S565730 <S565730@nwmissouri.edu>
2023-11-28 11:42:48 -0600

    COMMIT ALL

--- models/jaguar.js
+++ models/jaguar.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const jaguarSchema = mongoose.Schema({
 jaguar_color: String,
-jaguar_breed: String,
-jaguar_price: Number
+jaguar_breed: {type:String,length:15},
+jaguar_price: {type:Number,min:1000,max:200000}
 })
 module.exports = mongoose.model("jaguar",jaguarSchema)
--- routes/jaguar.js
+++ routes/jaguar.js
@@ -7,11 +7,18 @@ router.get('/', function(req, res, next) {
 var express = require('express');
 const jaguar_controlers= require('../controllers/jaguar');
 var router = express.Router();
+const secured = (req, res, next) => {
+  if (req.user){
+  return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+  }
 /* GET jaguars */
 router.get('/', jaguar_controlers.jaguar_view_all_Page );
-router.get('/detail', jaguar_controlers.jaguar_view_one_Page);
-router.get('/create', jaguar_controlers.jaguar_create_Page);
-router.get('/update', jaguar_controlers.jaguar_update_Page);
-router.get('/delete', jaguar_controlers.jaguar_delete_Page);
+router.get('/detail', secured, jaguar_controlers.jaguar_view_one_Page);
+router.get('/create', secured, jaguar_controlers.jaguar_create_Page);
+router.get('/update', secured, jaguar_controlers.jaguar_update_Page);
+router.get('/delete', secured,jaguar_controlers.jaguar_delete_Page);
 
 module.exports = router;
